c410af758a89394d6108dea20943f5ce7ca97d9e
Danilo Nascimento Cunha <id4ni10@gmail.com>
2019-05-17 11:32:21 -0300

    Update main.py

--- opencv-py/main.py
+++ opencv-py/main.py
@@ -3,7 +3,7 @@ import numpy
  
 cam = cv2.VideoCapture(0)
 kernel = numpy.ones((5 ,5), numpy.uint8)
- 
+
 if cam is None or not cam.isOpened():
     print('Warning: unable to open video source: ', cam)
 else:
@@ -13,12 +13,12 @@ else:
         rangomin = numpy.array([51, 0, 0])
         mask = cv2.inRange(frame, rangomin, rangomax)
         # reduce the noise
-        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    
+        opening = cv2.morphologyEx(mask, cv2.MORPH_BLACKHAT, kernel)
+
         x, y, w, h = cv2.boundingRect(opening)
-    
+
         cv2.rectangle(frame, (x, y), (x+w, y + h), (0, 255, 0), 3)
-        cv2.circle(frame, (x+w/2, y+h/2), 5, (0, 0, 255), -1)
+        cv2.circle(frame, (int(x+w/2), int(y+h/2)), 5, (0, 0, 255), -1)
     
         cv2.imshow('camera', frame)
     
